68ef207b159ff441dd62caed3f1c68481e528a3e
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-04 16:33:45 +1100

    Update storybook config to support scss

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,6 @@
 import { configure } from '@storybook/vue';
 
+import "../src/voltage/scss/voltage.scss";
+
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
