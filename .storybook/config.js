9c37d562c7d35d4471a5c9e7a16e3f7a1f06cfbc
HurricaneInteractive <hurricane.interactive@gmail.com>
2019-11-05 11:34:14 +1100

    Added storybook scss

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,5 +2,7 @@ import { configure } from '@storybook/vue';
 
 import "../src/voltage/scss/voltage.scss";
 
+import "../src/scss/storybook.scss";
+
 // automatically import all files ending in *.stories.js
 configure(require.context('../stories', true, /\.stories\.js$/), module);
